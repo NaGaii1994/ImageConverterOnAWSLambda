7856778a9fc871934d63ab884dfd4ed19ef4b8ea
NaGaii1994 <nagai_akihiro@icloud.com>
2023-04-03 14:12:34 +0000

    feat: Add the convert_image_to_webp function.
    To convert the image send to s3 to webp file.

--- lambda/main.py
+++ lambda/main.py
@@ -1,6 +1,9 @@
+import io
 import json
+import os
 
 import boto3
+from PIL import Image
 
 # バケット名,オブジェクト名
 BUCKET_NAME = "test-backet"
@@ -25,3 +28,25 @@ def list_backet_images():
             obj["LastModified"].strftime("%Y/%m/%d %H:%M:%S"),
         )
     return keys
+
+
+def convert_image_to_webp(key):
+    # Detect extension
+    ext = os.path.splitext(key)[1][1:]
+
+    if ext in ["jpg", "jpeg", "gif", "png"]:
+        s3_object = s3_client.get_object(Bucket=BUCKET_NAME, Key=key)
+        image_data = io.BytesIO(s3_object["Body"].read())
+        pil_image = Image.open(image_data)
+
+        # Save to memory
+        buffer = io.BytesIO()
+        pil_image.save(buffer, "WebP")
+
+        new_key = os.path.splitext(key)[0] + ".webp"
+
+        s3_client.put_object(
+            Bucket=BUCKET_NAME,
+            Key=new_key,
+            Body=buffer.getvalue(),
+        )
