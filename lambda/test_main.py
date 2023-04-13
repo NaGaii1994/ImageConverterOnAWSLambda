0a4384686aefa091a35a24b3c421e972f439841e
NaGaii1994 <nagai_akihiro@icloud.com>
2023-04-13 14:44:55 +0000

    chore: remove the unused list_bucket_images() function from codebase.

--- lambda/test_main.py
+++ lambda/test_main.py
@@ -19,23 +19,12 @@ def setup():
         CreateBucketConfiguration={"LocationConstraint": region_name},
     )
     transfer = S3Transfer(client)
-    for i in range(5):
-        transfer.upload_file(f"sample/{i}.jpg", main.BUCKET_NAME, f"{i}.jpg")
+    transfer.upload_file("sample/0.jpg", main.BUCKET_NAME, "0.jpg")
 
     yield client
     print("delete s3 backet...")
-    for i in range(5):
-        client.delete_object(Bucket=main.BUCKET_NAME, Key=f"{i}.jpg")
+    client.delete_object(Bucket=main.BUCKET_NAME, Key="0.jpg")
     client.delete_bucket(Bucket=main.BUCKET_NAME)
-
-
-def test_list_backet_images(setup):
-    main.s3_client = setup
-    assert "0.jpg" in main.list_backet_images()
-    assert "1.jpg" in main.list_backet_images()
-    assert "2.jpg" in main.list_backet_images()
-    assert "3.jpg" in main.list_backet_images()
-    assert "4.jpg" in main.list_backet_images()
 
 
 def test_convert_image_to_webp(setup):
