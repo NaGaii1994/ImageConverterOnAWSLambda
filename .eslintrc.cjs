dedd8f24b66f56a38f23d8aa7198fd3c6be48486
NaGaii1994 <nagai_akihiro@icloud.com>
2023-04-19 15:53:48 +0000

    feat: install react

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:unicorn/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          kebabCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        allowList: {
+          env: true,
+        },
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: ['vite.config.ts', 'dist'],
+};
